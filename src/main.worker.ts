e81fdccfa35e5ad4d4faabe03d71c2992d9c2b26
Rob Wormald <robwormald@gmail.com>
2018-08-27 13:23:43 -0700

    add hosting.

--- src/main.worker.ts
+++ src/main.worker.ts
@@ -3,23 +3,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 declare var importScripts;
 
 
 if (environment.production) {
   enableProdMode();
 }
+
 load();
 
-
-
 function load() {
-  importScripts('http://localhost:4200/zone.js');
+  const zoneUrl = `https://ng-webworker-demo.firebaseapp.com/zone.js`;
+  importScripts(zoneUrl);
   platformBrowserDynamic().bootstrapModule(AppModule)
-    .then(moduleRef => {
-      console.log(moduleRef);
-    })
-    .catch(err => console.log(err));
+   .catch(err => console.log(err));
 }
 
 
